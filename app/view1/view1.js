7808ec37783f2559bf67b6916d11356545819b85
ClementChaumel <clement.chaumel@gmail.com>
2017-01-11 13:59:23 +0100

    Landing page + map integration

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -11,4 +11,26 @@ angular.module('myApp.view1', ['ngRoute'])
 
 .controller('View1Ctrl', [function() {
 
+	function InitialiserCarte() {
+
+    var map = L.map('map').setView([43.6008028, 1.3626289], 12);
+
+    // create the tile layer with correct attribution
+    var tuileUrl = 'http://{s}.tile.osm.org/{z}/{x}/{y}.png';
+
+    var attrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+
+    var osm = L.tileLayer(tuileUrl, {
+        minZoom: 8,
+        maxZoom: 17,
+        attribution: attrib
+    });
+
+    osm.addTo(map);
+
+    var marker = L.marker([45.7531152, 4.827906]).addTo(map);
+}
+
+	InitialiserCarte();
+
 }]);
